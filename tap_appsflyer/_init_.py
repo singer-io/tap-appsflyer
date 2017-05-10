763e71783831e3d261ed9414286f3e56a19a4b23
Matthew Bellantoni <mjbellantoni@gmail.com>
2017-05-10 15:23:11 -0400

    Implement the "In App Events" Raw Data Report

--- tap_appsflyer/_init_.py
+++ tap_appsflyer/_init_.py
@@ -30,7 +30,8 @@ STATE = {}
 
 
 ENDPOINTS = {
-    "installs": "/export/{app_id}/installs_report/v5"
+    "installs": "/export/{app_id}/installs_report/v5",
+    "in_app_events": "/export/{app_id}/in_app_events_report/v5"
 }
 
 
@@ -244,6 +245,7 @@ def sync_installs():
 
     from_datetime = get_start("installs")
     to_datetime = datetime.datetime.now()
+
     if to_datetime < from_datetime:
         LOGGER.error("to_datetime (%s) is less than from_endtime (%s).", to_datetime, from_datetime)
         return
@@ -283,8 +285,141 @@ def sync_installs():
     singer.write_state(STATE)
 
 
+def sync_in_app_events():
+
+    schema = load_schema("raw_data/in_app_events")
+    singer.write_schema("installs", schema, ["id"])
+
+    # This order matters
+    fieldnames = (
+        "attributed_touch_type",
+        "attributed_touch_time",
+        "install_time",
+        "event_time",
+        "event_name",
+        "event_value",
+        "event_revenue",
+        "event_revenue_currency",
+        "event_revenue_usd",
+        "event_source",
+        "is_receipt_validated",
+        "af_prt",
+        "media_source",
+        "af_channel",
+        "af_keywords",
+        "campaign",
+        "af_c_id",
+        "af_adset",
+        "af_adset_id",
+        "af_ad",
+        "af_ad_id",
+        "af_ad_type",
+        "af_siteid",
+        "af_sub_siteid",
+        "af_sub1",
+        "af_sub2",
+        "af_sub3",
+        "af_sub4",
+        "af_sub5",
+        "af_cost_model",
+        "af_cost_value",
+        "af_cost_currency",
+        "contributor1_af_prt",
+        "contributor1_media_source",
+        "contributor1_campaign",
+        "contributor1_touch_type",
+        "contributor1_touch_time",
+        "contributor2_af_prt",
+        "contributor2_media_source",
+        "contributor2_campaign",
+        "contributor2_touch_type",
+        "contributor2_touch_time",
+        "contributor3_af_prt",
+        "contributor3_media_source",
+        "contributor3_campaign",
+        "contributor3_touch_type",
+        "contributor3_touch_time",
+        "region",
+        "country_code",
+        "state",
+        "city",
+        "postal_code",
+        "dma",
+        "ip",
+        "wifi",
+        "operator",
+        "carrier",
+        "language",
+        "appsflyer_id",
+        "advertising_id",
+        "idfa",
+        "android_id",
+        "customer_user_id",
+        "imei",
+        "idfv",
+        "platform",
+        "device_type",
+        "os_version",
+        "app_version",
+        "sdk_version",
+        "app_id",
+        "app_name",
+        "bundle_id",
+        "is_retargeting",
+        "retargeting_conversion_type",
+        "af_attribution_lookback",
+        "af_reengagement_window",
+        "is_primary_attribution",
+        "user_agent",
+        "http_referrer",
+        "original_url",
+    )
+
+    from_datetime = get_start("in_app_events")
+    to_datetime = datetime.datetime.now()
+
+    if to_datetime < from_datetime:
+        LOGGER.error("to_datetime (%s) is less than from_endtime (%s).", to_datetime, from_datetime)
+        return
+
+    params = dict()
+    params["from"] = from_datetime.strftime("%Y-%m-%d %H:%M")
+    params["to"] = to_datetime.strftime("%Y-%m-%d %H:%M")
+    params["api_token"] = CONFIG["api_token"]
+
+    url = get_url("in_app_events", app_id=CONFIG["app_id"])
+    request_data = request(url, params)
+
+    csv_data = RequestToCsvAdapter(request_data)
+    reader = csv.DictReader(csv_data, fieldnames)
+
+    next(reader) # Skip the heading row
+
+    # AppsFlyer returns records in order of most recent first. So, we
+    # need to reverse them in order to provide sensible STATE
+    # checkpoints.  According to the API documentation, there may be
+    # as many as 200,000 rows here but I don't have a better solution.
+    rows = []
+    for row in reader:
+        rows.append(row)
+    rows = reversed(rows)
+
+    # Emit updated records with state checkpoint
+    for i, row in enumerate(rows):
+        record = xform(row, schema)
+        singer.write_record("in_app_events", record)
+        utils.update_state(STATE, "in_app_events", record["event_time"])  # NOTE: This is different in each report
+
+        if (i % 250) == 0:
+            singer.write_state(STATE)
+
+    # Write out final state
+    singer.write_state(STATE)
+
+
 STREAMS = [
-    Stream("installs", sync_installs)
+    Stream("installs", sync_installs),
+    Stream("in_app_events", sync_in_app_events)
 ]
 
 
--- tests/fake_server.py
+++ tests/fake_server.py
@@ -1,5 +1,6 @@
 import datetime
 import io
+import json
 import random
 import re
 import uuid
@@ -120,7 +121,7 @@ def generate_installs_report(app_id, start_datetime, end_datetime):
 
         # Attributed Touch Type, Attributed Touch Time
         record[0] = random.choice(["click", "impression", "tv"])
-        record[1] = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        record[1] = current_datetime.strftime("%Y-%m-%d %H:%M:%S") # Sorted on this field
 
         # Install Time & #
         record[2] = (current_datetime + datetime.timedelta(seconds=47)).strftime("%Y-%m-%d %H:%M:%S")
@@ -198,25 +199,157 @@ def generate_installs_report(app_id, start_datetime, end_datetime):
             return
 
 
+def generate_in_app_events_report(app_id, start_datetime, end_datetime):
 
-@route("/export/<app_id>/installs_report/v5")
-def get_installs_report(app_id):
+    fake = Factory.create()
 
+    current_datetime = end_datetime
+
+    yield headings
+
+    for _ in range(200, 300):
+        record = [""] * len(headings) # Report has 81 columns
+
+        # Attributed Touch Type, Attributed Touch Time
+        record[0] = random.choice(["click", "impression", "tv"])
+        record[1] = (current_datetime - datetime.timedelta(days=47)).strftime("%Y-%m-%d %H:%M:%S")
+
+        # Install Time
+        record[2] = record[1]
+
+        # Event Time, Event Name, Event Source
+        record[3] = current_datetime.strftime("%Y-%m-%d %H:%M:%S") # Sorted on this field
+        record[4] = "af_purchase"
+
+        # Event Value, Event Revenue, Event Revenue USD
+        revenue = round(random.uniform(50.0, 2000.0), 2)
+        record[5] = json.dumps({'af_revenue': revenue })
+        record[6] = str(revenue)
+        record[8] = str(revenue)
+
+        # Event Source
+        record[9] = random.choice(["SDK", "S2S"])
+
+        # Media Source, Keywords, Campaign, Campaign ID, Adset, Adset ID
+        record[12] = "_".join(fake.words(3))
+        record[14] = " ".join(fake.words(random.randint(1, 4)))
+        record[15] = random.choice(["mobile", "web", "social"])
+        record[16] = str(random.randint(1000000, 2000000))
+        record[17] = random.choice(["mobile", "web", "social"])
+        record[18] = str(random.randint(1000000, 2000000))
+
+        # Region, Country Code, State, City, Postal Code
+        record[47] = "NA"
+        record[48] = "US"
+        record[49] = fake.state_abbr()
+        record[50] = fake.city()
+        record[51] = fake.zipcode()
+
+        # DMA, IP, WIFI
+        record[52] = str(random.randint(100, 999))
+        record[53] = ".".join([str(random.randint(100, 255))] * 4)
+        record[54] = random.choice(["true", "false"])
+
+        # Language
+        record[57] = "en-US"
+
+        # AppsFlyer ID
+        record[58] = "1493" + str(random.randint(100000, 999999)) + "000-" + str(random.randint(100000, 999999))
+
+        # IDFA, Customer User ID, IDFV
+        record[60] = random.choice(["", str(uuid.uuid4())])
+        record[62] = str(random.randint(1, 999999))
+        record[64] = str(uuid.uuid4())
+
+        # Platform, Device Type, OS Version
+        record[65] = "ios"
+        record[66] = "iPhone " + random.choice(["5", "6", "6 Plus", "7"])
+        record[67] = random.choice(["10.2", "10.2.1", "10.3.1", "9.3.1"])
+
+        # App Version
+        record[68] = random.choice(["1.2", "1.3", "1.4"])
+
+        # SDK Version
+        record[69] = "v4.4.6.3"
+
+        # App ID, App Name, Bundle ID
+        record[70] = app_id
+        record[71] = "Acme - Good Stuff"
+        record[72] = "com.example.AcmeApp"
+
+        # Is Retargeting
+        record[73] = "false"
+
+        # Attribution Lookback
+        record[75] = random.choice(["", "7d", "30d"])
+
+        # Is Primary Attribution
+        record[77] = "true"
+
+        # User Agent, HTTP Referrer, Original URL
+        record[78] = "Acme/1 CFNetwork/808.3 Darwin/16.3.0"
+        record[79] = random.choice(["", fake.uri()])
+        record[80] = random.choice(["", fake.uri()])
+
+        yield record
+
+        current_datetime = current_datetime - datetime.timedelta(minutes=47)
+
+        # Make sure we're not creating records out of the time range.
+        if current_datetime < start_datetime:
+            return
+
+
+def validate_app_id(app_id):
     m = re.match(r"\Aid[0-9]{10}\Z", app_id)
     if m is None:
         print("Invalid app_id")
         abort(500, "Invalid app_id")
 
-    m = re.match(r"\A[0-9a-f-]{36}\Z", request.query.api_token)
+
+def validate_api_token(api_token):
+    m = re.match(r"\A[0-9a-f-]{36}\Z", api_token)
     if m is None:
         print("Invalid api_token")
         abort(500, "Invalid api_token")
+
+
+@route("/export/<app_id>/installs_report/v5")
+def get_installs_report(app_id):
+
+    validate_app_id(app_id)
+    validate_api_token(request.query.api_token)
 
     start_datetime, end_datetime = extract_from_and_to_datetimes(request)
 
     output = io.StringIO()
 
     for row in generate_installs_report(app_id, start_datetime, end_datetime):
+        output.write(",".join(row) + "\n")
+
+    body = output.getvalue()
+    output.close()
+
+    headers = dict()
+    # "Date" and "Content-Length" get set for us
+    headers["Content-Disposition"] = "attachment;filename=id1234567890_installs_2017-04-26_2017-04-27_HONHH.csv"
+    # Not sure this is correct, but it's what the API returns
+    headers["Content-Type"] = "application/vnd.ms-excel; charset=UTF-8"
+
+    return HTTPResponse(body, **headers)
+
+
+@route("/export/<app_id>/in_app_events_report/v5")
+def get_in_app_events_report_report(app_id):
+
+    validate_app_id(app_id)
+    validate_api_token(request.query.api_token)
+
+    start_datetime, end_datetime = extract_from_and_to_datetimes(request)
+
+    output = io.StringIO()
+
+    for row in generate_in_app_events_report(app_id, start_datetime, end_datetime):
         output.write(",".join(row) + "\n")
 
     body = output.getvalue()
