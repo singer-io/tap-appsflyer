387ff926006d4f3867da470a1127f39953178561
Kyle Allan <kyle@stitchdata.com>
2018-04-03 16:22:35 -0400

    bump to 0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 0.0.2
+  * Updated the schema for in_app_events to set `customer_user_id` to allow a string value [#4](https://github.com/singer-io/tap-appsflyer/pull/4)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tap-appsflyer',
-    version='0.0.1',
+    version='0.0.2',
     description='Singer.io tap for extracting data from the AppsFlyer API',
     author='Stitch, Inc.',
     url='http://singer.io',
