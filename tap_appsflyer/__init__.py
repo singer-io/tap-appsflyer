ef444825151fb412009ad150993a8cbdd3e04f62
pbegle <peter.begle@bytecode.io>
2021-07-19 16:18:31 -0700

    Added helper function to remove whitespaces from config, added corresponding unittest.

--- tap_appsflyer/__init__.py
+++ tap_appsflyer/__init__.py
@@ -35,6 +35,16 @@ ENDPOINTS = {
 }
 
 
+def clean_config(config: dict) -> dict:
+    """Strips whitespace from any values in the config."""
+    for key in config.keys():
+        value = config[key]
+        if isinstance(value, str):
+            config[key] = value.strip()
+
+    return config
+
+
 def af_datetime_str_to_datetime(s):
     return datetime.datetime.strptime(s.strip(), "%Y-%m-%d %H:%M:%S")
 
@@ -465,7 +475,8 @@ def main():
             "api_token",
         ])
 
-    CONFIG.update(args.config)
+    config = clean_config(args.config)
+    CONFIG.update(config)
 
     if args.state:
         STATE.update(args.state)
