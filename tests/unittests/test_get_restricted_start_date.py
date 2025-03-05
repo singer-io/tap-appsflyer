d20119095015d681826813d2319ffd05c7bd2418
Rutuja Deshmukh <rutuja.deshmukh@qlik.com>
2025-03-05 17:39:57 +0000

    unit test fixture

--- tests/unittests/test_get_restricted_start_date.py
+++ tests/unittests/test_get_restricted_start_date.py
@@ -10,7 +10,7 @@ from tap_appsflyer.streams.abstracts import IncrementalStream
 class TestGetRestrictedStartDate(unittest.TestCase):
     def test_get_restricted_start_date(self):
         # Define the start date
-        date = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d %H-%M-%S")
+        date = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S")
 
         # Set the restriction_date to 90 days ago from the current UTC date
         restriction_date = datetime.datetime.now(pytz.utc) - datetime.timedelta(days=90)
