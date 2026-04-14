a987339ae7f9b15b3c42e21db9111c68b4d0f84a
Tushar Mittal <tushar.mittal@qlik.com>
2026-04-14 09:28:21 +0530

    parent-tap-stream-id addition

--- tap_appsflyer/schema.py
+++ tap_appsflyer/schema.py
@@ -67,6 +67,10 @@ def get_schemas() -> Tuple[Dict, Dict]:
                     mdata, ("properties", field_name), "inclusion", "automatic"
                 )
 
+        parent_tap_stream_id = getattr(stream_obj, 'parent', None)
+        if parent_tap_stream_id:
+            mdata = metadata.write(mdata, (), 'parent-tap-stream-id', parent_tap_stream_id)
+
         mdata = metadata.to_list(mdata)
         field_metadata[stream_name] = mdata
 
