e053caccedaf089ed2398afb11ac72f71056c584
Vankayalapati Rahul <vankayalapati.rahul@globallogic.com>
2026-06-26 10:44:07 +0530

    adding mock integrations

--- tests/unittests/test_helpers.py
+++ tests/unittests/test_helpers.py
@@ -9,7 +9,7 @@ from tap_appsflyer import clean_config, get_restricted_start_date
 class TestGetRestrictedStartDate(unittest.TestCase):
     def test_get_restricted_start_date(self):
         # Define the start date
-        date = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d %H-%M-%S")
+        date = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
 
         # Set the restriction_date to 90 days ago from the current UTC date
         restriction_date = datetime.datetime.now(pytz.utc) - datetime.timedelta(days=90)
